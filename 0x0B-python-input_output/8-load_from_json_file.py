19a384ac01511692279ce0ae57e714cb9d44abb1
Dalex702 <102964343+Dalex702@users.noreply.github.com>
2023-04-13 21:47:58 +0100

    8-load_from_json_file.py

---
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import json
+
+"""loads an object from json file"""
+
+def load_from_json_file(filename):
+
+    """does it"""
+
+    with open(filename, mode='r', encoding='utf-8') as f:
+
+        return(json.load(f))
+
